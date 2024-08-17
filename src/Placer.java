12d39694e4051710219b3d77e531d71c402994da
TotoroTron <brian.cheng.1997@gmail.com>
2024-08-17 13:55:00 -0400

    minor bug fixes

--- src/Placer.java
+++ src/Placer.java
@@ -1,12 +1,16 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import java.io.IOException;
 import java.io.BufferedWriter;
 
 import com.xilinx.rapidwright.design.Design;
 import com.xilinx.rapidwright.design.Net;
+import com.xilinx.rapidwright.design.Module;
+import com.xilinx.rapidwright.design.ModuleInst;
+
 import com.xilinx.rapidwright.device.Device;
 
 
@@ -19,25 +23,30 @@ public abstract class Placer {
     protected static final String synthesizedDcp = rootDir.concat("/outputs/synthesized.dcp");
     protected static final String placedDcp = rootDir.concat("/outputs/placed.dcp");
 
+
     public void init(BufferedWriter writer) throws IOException {
         Design design = Design.readCheckpoint(synthesizedDcp);
-        printDesignInfo(design);
+        printDesignInfo(writer, design);
         design = place(design); 
         design.writeCheckpoint(placedDcp);
     }
 
-    public void printDesignInfo(Design design) {
 
-        writer.write("Printing ModuleInst(s)...")
-        for (ModuleInst mi : design.getModuleInsts()) {
+    public void printDesignInfo(BufferedWriter writer, Design design) throws IOException {
+        Collection<ModuleInst> mis = design.getModuleInsts();
+        writer.write("Printing ModuleInst(s)...\n");
+        if (mis.isEmpty()) {
+            writer.write("Collection<ModuleInst> is empty!\n");
+            return;
+        }
+        for (ModuleInst mi : mis) { 
             Module module = mi.getModule();
             String moduleName = module.getName();
-            writer.write("Module name: ".concat(moduleName))
+            writer.write("Module name: ".concat(moduleName));
         }
     }
 
-    private abstract Design place(Design design); 
 
-
+    protected abstract Design place(Design design); 
 
 }
