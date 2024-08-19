e7a9426bab2b3e203afb6238a4a95c9f117de961
TotoroTron <brian.cheng.1997@gmail.com>
2024-08-19 17:28:11 -0400

    more work

--- src/Placer.java
+++ src/Placer.java
@@ -14,8 +14,12 @@ import com.xilinx.rapidwright.design.ModuleInst;
 import com.xilinx.rapidwright.design.ModuleImpls;
 import com.xilinx.rapidwright.design.Cell;
 
+
+import com.xilinx.rapidwright.edif.EDIFLibrary;
 import com.xilinx.rapidwright.edif.EDIFNetlist;
+import com.xilinx.rapidwright.edif.EDIFCell;
 import com.xilinx.rapidwright.edif.EDIFCellInst;
+import com.xilinx.rapidwright.edif.EDIFHierCellInst;
 
 import com.xilinx.rapidwright.device.Device;
 
@@ -40,13 +44,39 @@ public abstract class Placer {
 
     public void printDesignInfo(BufferedWriter writer, Design design) throws IOException {
         // https://www.rapidwright.io/javadoc/overview-tree.html
-        //
 
+        // Top level object for a logical EDIF netlist.
         EDIFNetlist netlist = design.getNetlist();
+
+        // Keeps track of a set of EDIFCell objects that are part of a netlist.
         EDIFLibrary library = netlist.getHDIPrimitivesLibrary();
+
+
+
+        // Logical cells in an EDIF netlist.
+        // Can be both a leaf cell or a hierarchical cell.
+        Collection<EDIFCell> cells = library.getCells();
+        writer.write("\n\nEDIF Cells: ");
+        for (EDIFCell cell : cells) {
+            writer.write("\n".concat(cell.getView()));
+        }
+
+
+
+        EDIFCell topCell = netlist.getTopCell();
+        writer.write("\nTop Cell: ".concat(topCell.getView()));
         
         List<EDIFCellInst> edifCellInsts = netlist.getAllLeafCellInstances();
-        List<EDIFHierCellInst> edifHeirCellInsts = netlist.getAllLeafHeirCellInstances();
+        writer.write("\n\nCell Instances: ");
+        for (EDIFCellInst eci : edifCellInsts) {
+            writer.write("\n".concat(eci.getCellName()));
+        }
+
+        List<EDIFHierCellInst> edifHeirCellInsts = netlist.getAllLeafHierCellInstances();
+        writer.write("\n\nCell Instances with Hierarchy: ");
+        for (EDIFHierCellInst ehci : edifHeirCellInsts) {
+            writer.write("\n".concat(ehci.getFullHierarchicalInstName()));
+        }
 
     }
 
