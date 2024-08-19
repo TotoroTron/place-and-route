8f6bae3654aad069dd3bb3f6934c1a71c04c91c9
TotoroTron <brian.cheng.1997@gmail.com>
2024-08-19 15:29:10 -0400

    minor work

--- src/Placer.java
+++ src/Placer.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -11,8 +12,10 @@ import com.xilinx.rapidwright.design.Net;
 import com.xilinx.rapidwright.design.Module;
 import com.xilinx.rapidwright.design.ModuleInst;
 import com.xilinx.rapidwright.design.ModuleImpls;
+import com.xilinx.rapidwright.design.Cell;
 
 import com.xilinx.rapidwright.edif.EDIFNetlist;
+import com.xilinx.rapidwright.edif.EDIFCellInst;
 
 import com.xilinx.rapidwright.device.Device;
 
@@ -36,27 +39,14 @@ public abstract class Placer {
 
 
     public void printDesignInfo(BufferedWriter writer, Design design) throws IOException {
-        Collection<ModuleImpls> mos = design.getModules();
-        writer.write("Printing Module(s)...\n");
-        if (mos.isEmpty()) {
-            writer.write("Collection<Module> is empty!\n");
-        }
-        for (ModuleImpls mo : mos) {
-            String moduleImplName = mo.getName();
-            writer.write("ModuleImpl name: ".concat(moduleImplName));
-        }
+        // https://www.rapidwright.io/javadoc/overview-tree.html
+        //
 
-
-        Collection<ModuleInst> mis = design.getModuleInsts();
-        writer.write("Printing ModuleInst(s)...\n");
-        if (mis.isEmpty()) {
-            writer.write("Collection<ModuleInst> is empty!\n");
-        }
-        for (ModuleInst mi : mis) { 
-            Module module = mi.getModule();
-            String moduleName = module.getName();
-            writer.write("Module name: ".concat(moduleName));
-        }
+        EDIFNetlist netlist = design.getNetlist();
+        EDIFLibrary library = netlist.getHDIPrimitivesLibrary();
+        
+        List<EDIFCellInst> edifCellInsts = netlist.getAllLeafCellInstances();
+        List<EDIFHierCellInst> edifHeirCellInsts = netlist.getAllLeafHeirCellInstances();
 
     }
 
--- tcl/synth.tcl
+++ tcl/synth.tcl
@@ -1,27 +1,23 @@
 # synth.tcl
 
 set root_dir "/home/bcheng/workspace/dev/place-and-route"
-# set viv_project_dir "$root_dir/hdl/vhdl/counter"
-# set viv_project_name "counter"
-set viv_design_name "top_level"
-set viv_synthesized_dcp "$root_dir/outputs/synthesized.dcp" 
+set design_name "top_level"
+set synthesized_dcp "$root_dir/outputs/synthesized.dcp" 
 
 set hdl_dir $root_dir/hdl/vhdl/counter/counter.srcs/sources_1/new
 set hdl_files [glob -nocomplain -directory $hdl_dir *.vhd]
 
-# open_project $viv_project_dir/$viv_project_name.xpr
 
 foreach file $hdl_files {
     puts "reading: $file"
     read_vhdl $file
 }
 
-synth_design -mode out_of_context -part xc7z020clg400-1 -top $viv_design_name
+synth_design -mode out_of_context -part xc7z020clg400-1 -top $design_name
 
-write_checkpoint -force $viv_synthesized_dcp
+write_checkpoint -force $synthesized_dcp
 
 exit
 
+# open project <proj.xpr>
 # close_project
-
-# puts "Synthesis complete. Check $viv_synthesized_dcp."
