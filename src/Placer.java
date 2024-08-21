3796755a7d6b29fa827c11170e3d3e8bd63b009d
TotoroTron <brian.cheng.1997@gmail.com>
2024-08-21 17:32:51 -0400

    idk

--- src/Placer.java
+++ src/Placer.java
@@ -45,10 +45,13 @@ public abstract class Placer {
         design.writeCheckpoint(placedDcp);
     }
 
-
+    // https://www.rapidwright.io/javadoc/overview-tree.html
+    // https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/design/Design.html
+    // https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/device/Device.html
+    // https://docs.amd.com/r/en-US/ug912-vivado-properties/CELL
+    
     public void printDesignInfo(BufferedWriter writer, Design design) throws IOException {
-        // https://www.rapidwright.io/javadoc/overview-tree.html
-        // https://docs.amd.com/r/en-US/ug912-vivado-properties/CELL
+
 
         // Top level object for a logical EDIF netlist.
         EDIFNetlist netlist = design.getNetlist();
@@ -56,46 +59,42 @@ public abstract class Placer {
         // Keeps track of a set of EDIFCell objects that are part of a netlist.
         EDIFLibrary library = netlist.getHDIPrimitivesLibrary();
 
-        /*
-         *
-         * These ideas are equivalent:
-         *      edif.EDIFCell   => design.Cell 
-         *      Logical Cell    => Physical Cell 
-         *      Post-Synth Cell => Post-Place Cell
-         * 
-         * EDIFCell: Represents a logical cell in an EDIF netlist.
-         * Cell: Corresponds to the leaf cell within the logical netlist EDIFCellInst and
-         *      provides a mapping to a physical location BEL on the device.
-         *      It could also be called a BELInst.
-         * 
-         * Synthesis provides a raw EDIF netlist with EDIFCellInst(s).
-         * Placement maps these logical EDIFCellInst(s) onto physical BELs / Tiles / Sites.
-         *      Cells are created during placement via design.createAndPlaceCell(...).
-         *      Example : Cell or2 = design.createAndPlaceCell("or2", Unisim.OR2, "SLICE_X112Y140/C6LUT");
-         *      Example : Cell led = design.createAndPlaceIOB("led", PinType.OUT, "R14", "LVCMOS33")
-         * 
-         * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/design/Cell.html
-         * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/edif/EDIFCell.html
-         *
-        */
-
-        EDIFCell topCell = netlist.getTopCell();
-        writer.write("\nTop Cell: "+topCell.getView());
-         
-        List<EDIFCellInst> edifCellInsts = netlist.getAllLeafCellInstances();
-        writer.write("\n\nCell Instances: ");
-        for (EDIFCellInst eci : edifCellInsts) {
-            writer.write("\n"+eci.getCellName());
+        List<EDIFHierCellInst> ehcis = netlist.getAllLeafHierCellInstances();
+        for (EDIFHierCellInst ehci : ehcis) {
+            writer.write("\nCell Name: "+ehci.getCellName());
+            writer.write("\n\tDepth: "+ehci.getDepth());
+            writer.write("\n\tHierarchical Inst Name:"+ehci.getFullHierarchicalInstName());
+            // EDIFHierNet ehn = ehci.getNet());
         }
-
-        List<EDIFHierCellInst> edifHeirCellInsts = netlist.getAllLeafHierCellInstances();
-        writer.write("\n\nCell Instances with Hierarchy: ");
-        for (EDIFHierCellInst ehci : edifHeirCellInsts) {
-            writer.write("\n"+ehci.getFullHierarchicalInstName());
-        }
-
     }
 
+    /*
+     *
+     * These ideas are equivalent:
+     *      edif.EDIFCell   => design.Cell 
+     *      Logical Cell    => Physical Cell 
+     *      Post-Synth Cell => Post-Place Cell
+     * 
+     * EDIFCell: Represents a logical cell in an EDIF netlist.
+     * Cell: Corresponds to the leaf cell within the logical netlist EDIFCellInst and
+     *      provides a mapping to a physical location BEL on the device.
+     *      It could also be called a BELInst.
+     * 
+     * Synthesis provides a raw EDIF netlist with EDIFCellInst(s).
+     * Placement maps these logical EDIFCellInst(s) onto physical BELs / Tiles / Sites.
+     *      Cells are created during placement via design.createAndPlaceCell(...).
+     *      Example : Cell or2 = design.createAndPlaceCell("or2", Unisim.OR2, "SLICE_X112Y140/C6LUT");
+     *      Example : Cell led = design.createAndPlaceIOB("led", PinType.OUT, "R14", "LVCMOS33")
+     * 
+     * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/design/Cell.html
+     * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/design/Unisim.html
+     * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/edif/EDIFCell.html
+     * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/edif/EDIFNetlist.html
+     * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/edif/EDIFHierCellInst.html
+     * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/edif/EDIFHierNet.html
+     * https://www.rapidwright.io/javadoc/com/xilinx/rapidwright/edif/EDIFHierPortInst.html
+     * 
+    */
 
     protected abstract Design place(Design design); 
 
