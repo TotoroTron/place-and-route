cd8fd53b0b2bc46e24f8555773d3e16ef59cc6f1
TotoroTron <brian.cheng.1997@gmail.com>
2024-09-28 14:26:27 -0400

    more random placer work. need to work on BEL packing into sites. Hierarchical decision Flip flops <= Carry4 <= LUTs

--- java/src/main/java/placer/PlacerRandom.java
+++ java/src/main/java/placer/PlacerRandom.java
@@ -39,68 +39,135 @@ public class PlacerRandom extends Placer {
         super();
     }
 
+    private void printAllCompatiblePlacements(BufferedWriter writer, Cell cell)
+            throws IOException {
+        writer.write("\n\tCompatible placements: ");
+        Map<SiteTypeEnum, Set<String>> compatibleBELs = cell.getCompatiblePlacements(this.device);
+
+        for (Map.Entry<SiteTypeEnum, Set<String>> entry : compatibleBELs.entrySet()) {
+            SiteTypeEnum siteType = entry.getKey();
+            Set<String> belNames = entry.getValue();
+
+            writer.write("\n\t\tSiteTypeEnum: " + siteType.name());
+            Site[] sites = device.getAllSitesOfType(siteType);
+
+            for (String bel : belNames)
+                writer.write("\n\t\t\tBEL: " + bel);
+            if (sites.length == 0) {
+                writer.write("\n\t\t\tSites: None!");
+                continue;
+            }
+            if (sites.length > 10) {
+                writer.write("\n\t\t\t" + sites.length + " compatible sites.");
+                continue;
+            }
+            for (Site site : sites)
+                writer.write("\n\t\t\tSite: " + site.getName());
+        }
+        return;
+    }
+
     public Design place(Design design) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(rootDir + "outputs/PlacerRandom.txt"));
         EDIFNetlist netlist = design.getNetlist();
-        List<Cell> cells = new ArrayList<>();
-        List<Net> nets = new ArrayList<>();
 
+        // CREATE NETS
         Map<EDIFHierNet, EDIFHierNet> edifNetMap = netlist.getParentNetMap();
-        List<EDIFHierCellInst> cellInstList = netlist.getAllLeafHierCellInstances();
+        for (EDIFHierNet ehn : edifNetMap.keySet()) {
+            design.createNet(ehn);
+        }
 
         // CREATE AND PLACE CELLS
+        List<EDIFHierCellInst> cellInstList = netlist.getAllLeafHierCellInstances();
         for (EDIFHierCellInst ehci : cellInstList) {
 
             Cell cell = design.createCell(ehci.getFullHierarchicalInstName(), ehci.getInst());
             Map<SiteTypeEnum, Set<String>> compatibleBELs = cell.getCompatiblePlacements(device);
-            writer.write("\nPlacing Cell: " + cell.getName());
 
-            if (!ehci.isTopLevelInst()) {
-                // If the EDIFHierCellInst is a Flip Flop type, getCompatiblePlacements will say
-                // that I/OLOGIC is compatible with it.
-                List<SiteTypeEnum> bufferTypes = new ArrayList<>();
-                Collections.addAll(bufferTypes,
-                        SiteTypeEnum.ILOGICE2,
-                        SiteTypeEnum.ILOGICE3,
-                        SiteTypeEnum.OLOGICE2,
-                        SiteTypeEnum.OLOGICE3);
-                // for (SiteTypeEnum ste : compatibleBELs.keySet()) {}
-                compatibleBELs.keySet().removeAll(bufferTypes);
-            }
+            writer.write("\nPlacing Cell: " + cell.getName());
+            printAllCompatiblePlacements(writer, cell);
+
+            List<SiteTypeEnum> sitelessTypes = new ArrayList<>();
+            Collections.addAll(sitelessTypes,
+                    SiteTypeEnum.ILOGICE2,
+                    SiteTypeEnum.ILOGICE3,
+                    SiteTypeEnum.OLOGICE2,
+                    SiteTypeEnum.OLOGICE3,
+                    SiteTypeEnum.IOB18,
+                    SiteTypeEnum.OPAD
+            // SiteTypeEnum.IPAD has sites
+            );
+            compatibleBELs.keySet().removeAll(sitelessTypes);
 
             Set<String> occupiedSiteBELs = new HashSet<>();
             int iterCount = 0;
             while (true) {
                 Random rand = new Random();
                 List<SiteTypeEnum> keys = new ArrayList<>(compatibleBELs.keySet());
+
+                if (keys.isEmpty()) {
+                    writer.write("\n\tCell: " + cell.getName() + " has no compatible BELs!");
+                    break;
+                }
+
                 SiteTypeEnum selectedSiteType = keys.get(rand.nextInt(keys.size())); // Randomly selected SiteTypeEnum
+                Site[] sites = device.getAllSitesOfType(selectedSiteType);
+
+                if (sites.length == 0) {
+                    writer.write(
+                            "\n\tSiteTypeEnum: " + selectedSiteType + " has no compatible sites on the device!");
+                    break;
+                }
 
                 Set<String> randBELSet = compatibleBELs.get(selectedSiteType); // Randomly selected Set<String>
                 List<String> randBELList = new ArrayList<>(randBELSet);
                 String selectedBELName = randBELList.get(rand.nextInt(randBELList.size()));
 
-                Site[] sites = device.getAllSitesOfType(selectedSiteType);
-                System.out.println("Cell: " + cell.getName());
-                System.out.println("Sites length: " + sites.length);
                 Site selectedSite = sites[rand.nextInt(sites.length)];
                 BEL selectedBEL = selectedSite.getBEL(selectedBELName);
 
                 if (occupiedSiteBELs.add(selectedSite.getName() + "_" + selectedBELName)) {
                     // Unique siteBEL pair successfully added to Set
                     design.placeCell(cell, selectedSite, selectedBEL);
-                    writer.write("Placed cell: " + cell.getName() + " at site: "
-                            + selectedSite.getName() + " at BEL: " + selectedBELName);
+                    writer.write("\n\tPLACED CELL: ");
+                    writer.write("\n\t\tBEL: " + selectedBELName);
+                    writer.write("\n\t\tSite: " + selectedSite.getName());
+                    break;
+                }
+                if (iterCount == 100) {
+                    writer.write("\n\tCould not place cell: " + cell.getName() + " after 100 random selections!");
                     break;
                 }
                 iterCount++;
-                if (iterCount == 100) {
-                    writer.write("\nCould not place cell: " + cell.getName() + " after 100 random selections!");
-                    break;
-                }
             }
+            writer.newLine();
 
-        }
+        } // end for (EDIFHierCellInst ehci : cellInstList)
 
+        if (writer != null)
+            writer.close();
         return design;
-    }
-}
+
+    } // end PlacerRandom()
+} // end class
+
+// String ehciName = ehci.getCellName();
+// if (ehciName == "OBUF" || ehciName == "IBUF") {
+// writer.write("\n\tOBUF/IBUF cell has no corresponding SiteTypeEnum!");
+// writer.write("\n\tis Top level? " + ehci.isTopLevelInst());
+// break;
+// }
+
+// if (!ehci.isTopLevelInst()) {
+// // If the EDIFHierCellInst is a Flip Flop type, getCompatiblePlacements will
+// say
+// // that I/OLOGIC is compatible with it.
+// List<SiteTypeEnum> bufferTypes = new ArrayList<>();
+// Collections.addAll(bufferTypes,
+// SiteTypeEnum.ILOGICE2,
+// SiteTypeEnum.ILOGICE3,
+// SiteTypeEnum.OLOGICE2,
+// SiteTypeEnum.OLOGICE3);
+// // for (SiteTypeEnum ste : compatibleBELs.keySet()) {}
+// compatibleBELs.keySet().removeAll(bufferTypes);
+// }
