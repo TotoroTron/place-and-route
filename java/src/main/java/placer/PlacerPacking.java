60271036a976e57419df50b9300263103b728d8c
TotoroTron <brian.cheng.1997@gmail.com>
2024-10-22 18:17:31 -0400

    more packer planning

--- java/src/main/java/placer/PlacerPacking.java
+++ java/src/main/java/placer/PlacerPacking.java
@@ -66,7 +66,45 @@ public class PlacerPacking extends Placer {
             Cell cell,
             Map<String, List<String>> occupiedPlacements) throws IOException {
 
-    }
+        Map<SiteTypeEnum, Set<String>> compatiblePlacements = cell.getCompatiblePlacements(device);
+        removeBufferTypes(compatiblePlacements.keySet());
+
+        // ABSTRACT
+        SiteTypeEnum selectedSiteType = selectSiteType(compatiblePlacements);
+
+        List<String> siteNames = Arrays.stream(device.getAllCompatibleSites(selectedSiteType))
+                .map(Site::getName) // return as string names only, not the site itself
+                .collect(Collectors.toList()); // collect as list
+        List<String> belNames = compatiblePlacements.get(selectedSiteType).stream()
+                .filter(name -> !name.contains("5FF")) // placing regs on 5FF BELs will cause routing problems
+                .collect(Collectors.toList()); // UG474, CH2 Storage Elements for more information
+        Map<String, List<String>> availablePlacements = new HashMap<>();
+        for (String siteName : siteNames) {
+            availablePlacements.put(siteName, new ArrayList<>(belNames));
+        }
+
+        // ABSTRACT
+        removeOccupiedPlacements(availablePlacements, occupiedPlacements);
+
+        // ABSTRACT
+        String[] selectedPlacement = selectSiteAndBEL(availablePlacements);
+
+        String selectedSiteName = selectedPlacement[0];
+        String selectedBELName = selectedPlacement[1];
+
+        System.out.println("Selected Site + BEL: " + selectedSiteName + ", " + selectedBELName);
+        Site selectedSite = device.getSite(selectedSiteName);
+        BEL selectedBEL = selectedSite.getBEL(selectedBELName);
+        if (design.placeCell(cell, selectedSite, selectedBEL)) {
+            String s1 = String.format(
+                    "\n\tcellName: %-40s Site: %-10s BEL: %-10s",
+                    cell.getName(), selectedSiteName, selectedBELName);
+            addToMap(occupiedPlacements, selectedPlacement[0], selectedPlacement[1]);
+        } else {
+            writer.write("\n\tWARNING: Placement Failed!");
+        }
+
+    } // end placeCell()
 
     public @Override void placeDesign() throws IOException {
         writer.write("\n\nPlacing Design...");
@@ -87,86 +125,56 @@ public class PlacerPacking extends Placer {
         }
 
         for (Cell cell : cells) {
-            String cellType = cell.getType();
-
             if (cell.getType() == "CARRY4") {
                 CARRYCells.add(cell);
                 cells.remove(cell);
-                /*
-                 * Find the input and output pins of this CARRY cell.
-                 *
-                 * Do the output pins connect to FF cells?
-                 * If so, find that FF cell and place it in the same site.
-                 *
-                 * Do the input pins connect to LUT cells?
-                 * If so, find that LUT cell and place it in the same site.
-                 */
             }
-
             if (cell.getType() == "FDRE") {
                 FFCells.add(cell);
                 cells.remove(cell);
-                /*
-                 * Find the input pins of the FF cell.
-                 * Do the input pins connect to LUTs?
-                 * If so, find that LUT and place it in the same site.
-                 */
             }
-
             if (cell.getType().contains("LUT")) {
                 LUTCells.add(cell);
                 cells.remove(cell);
-                /*
-                 * By now, most LUTs should be placed.
-                 * If this LUT connects to other LUTs,
-                 * try to place them in an *adjacent* site
-                 */
             }
-
         }
 
-        for (Cell cell : cells) {
+        for (Cell CARRYCell : CARRYCells) {
+            /*
+             * Find the input and output pins of this CARRY cell.
+             *
+             * First, check if this carry-in is sourced by another carry or if carry-out
+             * is the source of another carry.
+             * Place those CARRY cells first in some recursive fashion.
+             * while(true) with break conditions?.
+             *
+             * Do the output pins connect to FF cells?
+             * If so, find that FF cell and place it in the same site.
+             *
+             * Do the input pins connect to LUT cells?
+             * If so, find that LUT cell and place it in the same site.
+             */
+            placeCell(CARRYCell, occupiedPlacements);
+        }
 
-            Map<SiteTypeEnum, Set<String>> compatiblePlacements = cell.getCompatiblePlacements(device);
-            removeBufferTypes(compatiblePlacements.keySet());
+        for (Cell FFCell : FFCells) {
+            /*
+             * Find the input pins of the FF cell.
+             * Do the input pins connect to LUTs?
+             * If so, find that LUT and place it in the same site.
+             */
+            placeCell(FFCell, occupiedPlacements);
+        }
 
-            // ABSTRACT
-            SiteTypeEnum selectedSiteType = selectSiteType(compatiblePlacements);
+        for (Cell LUTCell : LUTCells) {
+            /*
+             * By now, most LUTs should already be placed.
+             * If this LUT connects to other LUTs,
+             * try to place them in an *adjacent* site
+             */
+            placeCell(LUTCell, occupiedPlacements);
+        }
 
-            List<String> siteNames = Arrays.stream(device.getAllCompatibleSites(selectedSiteType))
-                    .map(Site::getName) // return as string names only, not the site itself
-                    .collect(Collectors.toList()); // collect as list
-            List<String> belNames = compatiblePlacements.get(selectedSiteType).stream()
-                    .filter(name -> !name.contains("5FF")) // placing regs on 5FF BELs will cause routing problems
-                    .collect(Collectors.toList()); // UG474, CH2 Storage Elements for more information
-            Map<String, List<String>> availablePlacements = new HashMap<>();
-            for (String siteName : siteNames) {
-                availablePlacements.put(siteName, new ArrayList<>(belNames));
-            }
+    } // end placeDesign()
 
-            // ABSTRACT
-            removeOccupiedPlacements(availablePlacements, occupiedPlacements);
-
-            // ABSTRACT
-            String[] selectedPlacement = selectSiteAndBEL(availablePlacements);
-
-            String selectedSiteName = selectedPlacement[0];
-            String selectedBELName = selectedPlacement[1];
-
-            System.out.println("Selected Site + BEL: " + selectedSiteName + ", " + selectedBELName);
-            Site selectedSite = device.getSite(selectedSiteName);
-            BEL selectedBEL = selectedSite.getBEL(selectedBELName);
-            if (design.placeCell(cell, selectedSite, selectedBEL)) {
-                String s1 = String.format(
-                        "\n\tcellName: %-40s Site: %-10s BEL: %-10s",
-                        cell.getName(), selectedSiteName, selectedBELName);
-                addToMap(occupiedPlacements, selectedPlacement[0], selectedPlacement[1]);
-            } else {
-                writer.write("\n\tWARNING: Placement Failed!");
-            }
-
-        } // end for(Cell)
-
-    } // end placDesign()
-
-}
+} // end class
