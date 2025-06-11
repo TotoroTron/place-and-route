5f35cce3bd7d9cb6b205a4d741a6aa46e39ce56a
TotoroTron <brian.cheng.1997@gmail.com>
2025-06-11 17:26:42 -0400

    start placement results

--- java/src/main/java/placer/Main.java
+++ java/src/main/java/placer/Main.java
@@ -81,10 +81,10 @@ public class Main {
             // works entirely on the SiteInst/Site/Tile level.
             List<PlacerAnnealRandom> SAPlacers = new ArrayList<PlacerAnnealRandom>();
             SAPlacers.add(new PlacerAnnealRandom(rootDir, design, device, region));
+            SAPlacers.add(new PlacerAnnealMidpoint(rootDir, design, device, region));
             SAPlacers.add(new PlacerAnnealHybrid(rootDir, design, device, region));
-            // SAPlacers.add(new PlacerAnnealMidpoint(rootDir, design, device, region));
-            // SAPlacers.add(new PlacerGreedyRandom(rootDir, design, device, region));
-            // SAPlacers.add(new PlacerGreedyMidpoint(rootDir, design, device, region));
+            SAPlacers.add(new PlacerGreedyRandom(rootDir, design, device, region));
+            SAPlacers.add(new PlacerGreedyMidpoint(rootDir, design, device, region));
 
             for (PlacerAnnealRandom placer : SAPlacers) {
                 System.out.println("\n\nStarting " + placer.getPlacerName() + "... \n\n");
